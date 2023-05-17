080d039456027c5d63402e195d69d49e3c13b516
Oskar <Oskar>
2023-05-17 21:17:15 +0200

    Fixing errors

--- reverseInteger.cpp
+++ reverseInteger.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <string.h>
 #include <string>
-
+#include <math.h>
 
 using namespace std;
 
 
 int reverseInteger(int number){
-
+    int result =0;
     string stringNumber = std::to_string(number);
     string reverseNumber;
 
     for (int i = stringNumber.length() - 1; i >= 0; i--)reverseNumber += stringNumber[i];
 
-    int result = std::stoi(reverseNumber);
+    try {
+        int result = std::stoi(reverseNumber);
+    }catch(exception){
+        cout<<"Error --> out of range"<<endl;
+        return 0;
+    }
 
+    if ((result>pow(2,31))||(result<pow(-2,31))){
+        return 0;
+    }
     return result;
 }
 
